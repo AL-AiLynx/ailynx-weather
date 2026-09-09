a8a727ea6544c36d8f73044e14b02ba88cbb1a15
AiLynx <sjjunsaxsmax@gmail.com>
2026-09-09 09:40:00 +0900

    sync: deploy verified admin auth runtime

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 +"use strict";
 
 const CACHE_NAME =
-  "ailynx-weather-v47";
+  "ailynx-weather-v48";
 
 const AS1_VALIDATION_ENDPOINT =
   "https://jggazwqwalincsjegieo.supabase.co/functions/v1/as1-validation-read";
