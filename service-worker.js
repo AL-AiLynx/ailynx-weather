a937aed22a9aae6e0e4a9d84a4318b99e75620a1
AiLynx <sjjunsaxsmax@gmail.com>
2026-09-09 12:54:17 +0900

    sync: unify weather UI consumption path

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 +"use strict";
 
 const CACHE_NAME =
-  "ailynx-weather-v53";
+  "ailynx-weather-v54";
 
 const AS1_VALIDATION_ENDPOINT =
   "https://jggazwqwalincsjegieo.supabase.co/functions/v1/as1-validation-read";
@@ -37,7 +37,8 @@ const APP_SHELL = [
   "./market-price-client.js?v=15",
   "./market-dominance-client.js?v=15",
   "./visit-counter-client.js?v=15",
-  "./app.js?v=39",
+  "./weather-view-model.js?v=1",
+  "./app.js?v=40",
   "./weather-data.json",
   "./horus-sample.json",
   "./manifest.webmanifest?v=2",
